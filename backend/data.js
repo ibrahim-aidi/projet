03f794994cde946dc1a7894d10d39fe11258a398
ibrahimaidi <aidi.ibrahim@etudiant-fst.utm.tn>
2022-11-15 23:06:32 +0100

    first commit

--- backend/data.js
+++ backend/data.js
@@ -1,71 +1,87 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
-   products: [
-     {
-       _id: '1',
-       name: 'Whey isolate',
-       category: 'whey ',
-       image: '/images/p1.jpg',
-       price: 240,countInStock: 15,
-       brand: 'Olymp',
-       rating: 4.5,
-       numReviews: 10,
-       description: 'high quality product',
-     },
-     {
-       _id: '2',
-       name: 'Creatine monohydrate',
-       category: 'creatine',
-       image: '/images/p2.jpg',
-      price: 150, countInStock: 0,
-       brand: 'ON',
-       rating: 4.0,
-       numReviews: 10,
-       description: 'high quality product',
-     },
-     {
-       _id: '3',
-       name: 'L-CARNITINE',
-       category: 'fat burner',
-       image: '/images/p3.jpg',
-       price: 220,countInStock: 10,
-       brand: 'ON',
-       rating: 3,
-       numReviews: 17,
-       description: 'high quality product',
-     },
-     {
-       _id: '4',
-       name: 'Mass gainer',
-       category: 'mass',
-       image: '/images/p4.jpg',
-       price: 78,countInStock: 25,
-       brand: 'ON',
-       rating: 2.5,
-       numReviews: 14,
-       description: 'high quality product',
-     },
-     {
-       _id: '5',
-       name: 'Lipo 6',
-       category: 'fat burner',
-       image: '/images/p5.jpg',
-       price: 65,countInStock: 45,
-       brand: 'Nutrex',
-       rating: 4.5,
-       numReviews: 10,
-       description: 'high quality product',
-     },
-     {
-       _id: '6',
-       name: 'Citruline',
-       category: '',
-       image: '/images/p6.jpg',
-       price: 139,countInStock: 5,
-       brand: 'OstroVit',
-       rating: 4.5,
-       numReviews: 15,
-       description: 'high quality product',
-     },
-   ],
- };
- export default data;
+  users: [
+    {
+      name: 'Basir',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      
+      name: 'Whey isolate',
+      category: 'whey ',
+      image: '/images/p1.jpg',
+      price: 240,countInStock: 15,
+      brand: 'Olymp',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      
+      name: 'Creatine monohydrate',
+      category: 'creatine',
+      image: '/images/p2.jpg',
+     price: 150, countInStock: 0,
+      brand: 'ON',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      
+      name: 'L-CARNITINE',
+      category: 'fat burner',
+      image: '/images/p3.jpg',
+      price: 220,countInStock: 10,
+      brand: 'ON',
+      rating: 3,
+      numReviews: 17,
+      description: 'high quality product',
+    },
+    {
+      
+      name: 'Mass gainer',
+      category: 'mass',
+      image: '/images/p4.jpg',
+      price: 78,countInStock: 25,
+      brand: 'ON',
+      rating: 2.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      
+      name: 'Lipo 6',
+      category: 'fat burner',
+      image: '/images/p5.jpg',
+      price: 65,countInStock: 45,
+      brand: 'Nutrex',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      
+      name: 'Citruline',
+      category: 'supplement',
+      image: '/images/p6.jpg',
+      price: 139,countInStock: 5,
+      brand: 'OstroVit',
+      rating: 4.5,
+      numReviews: 15,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,31 @@
 import express from 'express';
-import data from './data.js';
+import mongoose from 'mongoose';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/mysupps', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
